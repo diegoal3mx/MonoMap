a5e0283d6e74abcf70f519e27e4a764104a34061
diegoal3mx <114199341+diegoal3mx@users.noreply.github.com>
2024-09-17 15:16:51 -0600

    Code cleaned

--- domain/templates/email.template.ts
+++ domain/templates/email.template.ts
@@ -1,81 +1,81 @@
 import { envs } from "../../src/config/envs.plugin";
 
-export function generateCaseEmailTemplate(creationDate: Date, genre:string, age:number, lat: number, lng: number): string {
-  const mapboxUrl = generateMapboxStaticImageURL(lat,lng)
-  return `
-  <!DOCTYPE html>
-  <html lang="es">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Detalles del Caso</title>
-      <style>
-          body {
-              font-family: Arial, sans-serif;
-              background-color: #f4f4f4;
-              color: #333;
-              margin: 0;
-              padding: 0;
-          }
-          .container {
-              width: 100%;
-              max-width: 600px;
-              margin: 20px auto;
-              background-color: #ffffff;
-              border-radius: 8px;
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-              overflow: hidden;
-          }
-          .header {
-              background-color: #007BFF;
-              color: #ffffff;
-              padding: 20px;
-              text-align: center;
-          }
-          .header h1 {
-              margin: 0;
-              font-size: 24px;
-          }
-          .content {
-              padding: 20px;
-          }
-          .content p {
-              margin: 10px 0;
-          }
-          .footer {
-              background-color: #f4f4f4;
-              color: #777;
-              padding: 10px;
-              text-align: center;
-              font-size: 12px;
-          }
-          .map-img{
-              width:100%;
-              height:auto;
-              border-radius: 10px;
-          }
-      </style>
-  </head>
-  <body>
-      <div class="container">
-          <div class="header">
-              <h1>Detalles del Caso</h1>
-          </div>
-          <div class="content">
-              <p><strong>Fecha:</strong> ${creationDate}</p>
-              <p><strong>Género:</strong> ${genre}</p>
-              <p><strong>Edad:</strong> ${age}</p>
-              <p><strong>Latitud:</strong> ${lat}</p>
-              <p><strong>Longitud:</strong> ${lng}</p>
-              <img src="${mapboxUrl}" class="map-img"/>
-          </div>
-          <div class="footer">
-              <p>Este es un correo generado automáticamente. Por favor, no responda a este mensaje.</p>
-          </div>
-      </div>
-  </body>
-  </html>
-  `;
+export function generateCaseEmailTemplate(creationDate: string, genre:string, age:number, lat: number, lng: number): string {
+const mapboxUrl = generateMapboxStaticImageURL(lat,lng)
+return `
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalles del Caso</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            width: 100%;
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #ffffff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+        }
+        .header {
+            background-color: #007BFF;
+            color: #ffffff;
+            padding: 20px;
+            text-align: center;
+        }
+        .header h1 {
+            margin: 0;
+            font-size: 24px;
+        }
+        .content {
+            padding: 20px;
+        }
+        .content p {
+            margin: 10px 0;
+        }
+        .footer {
+            background-color: #f4f4f4;
+            color: #777;
+            padding: 10px;
+            text-align: center;
+            font-size: 12px;
+        }
+        .map-img{
+            width:100%;
+            height:auto;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Detalles del Caso</h1>
+        </div>
+        <div class="content">
+            <p><strong>Fecha:</strong> ${creationDate}</p>
+            <p><strong>Género:</strong> ${genre}</p>
+            <p><strong>Edad:</strong> ${age}</p>
+            <p><strong>Latitud:</strong> ${lat}</p>
+            <p><strong>Longitud:</strong> ${lng}</p>
+            <img src="${mapboxUrl}" class="map-img"/>
+        </div>
+        <div class="footer">
+            <p>Este es un correo generado automáticamente. Por favor, no responda a este mensaje.</p>
+        </div>
+    </div>
+</body>
+</html>
+`;
 }
 export const generateMapboxStaticImageURL= (lat:number, lng:number) =>{
   const accessToken = envs.MAPBOX_ACCESS_TOKEN; // Reemplaza con tu token de acceso de Mapbox
@@ -83,5 +83,5 @@ export const generateMapboxStaticImageURL= (lat:number, lng:number) =>{
   const width = 800; // Ancho de la imagen
   const height = 500; // Altura de la imagen
 
-  return `https://api.mapbox.com/styles/v1/mapbox/light-v11/static/pin-l-embassy+f74e4e(${lng},${lat})/${lng},${lat},${zoom}/${width}x${height}?access_token=${accessToken}`;
+return `https://api.mapbox.com/styles/v1/mapbox/light-v11/static/pin-l-embassy+f74e4e(${lng},${lat})/${lng},${lat},${zoom}/${width}x${height}?access_token=${accessToken}`;
 }
